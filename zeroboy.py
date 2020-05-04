be2cf41c718f85f211ea00841bef2a108127e7b3
kfpori365 <64778970+kfpori365@users.noreply.github.com>
2020-05-04 15:16:27 +0400

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -25,7 +25,7 @@ def acak(b):
     w = 'ahtdzjc'
     d = ''
     for i in x:
-        d += '!'+w[random.randint(0,len(w)-1)]+i
+        d += '!'+w[random.randint(0,len(w)-1)]+
     return cetak(d)
 
 
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - KHURSHED-OFFICAL------•◈•
+|__\~~) •||•KFPORI --ALAM---------------•◈•
+|__(-----\  •◈•------BLACK--POWER-------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKhurshed Alam
+\033[1;96mInstagram \033[1;93m: \033[1;FlowMfa.Khurshed
+\033[1;96mFacebook  \033[1;93m: \033[1; Adel.Vaiya
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/kfpori365
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKHURSHED ALAM
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowMfa.khurshed
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Adel.Vaiya
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/kfpori365
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "khurshed"
+CorrectPassword = "khurshed"
 
 loop = 'true'
 while (loop == 'true'):
